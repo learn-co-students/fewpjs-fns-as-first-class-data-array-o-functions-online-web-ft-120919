b365c922837b548876eb30715c79e78c1b41af6a
93geraldtee <93geraldtee@gmail.com>
2020-04-04 14:23:18 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function wakeDog(dogName, dogBreed) {
+    const dogAction  = `Wake ${dogName} the ${dogBreed}`;
+    console.log(dogAction);
+    return dogAction
+  }
+function leashDog(dogName, dogBreed) {
+    const dogAction = `Leash ${dogName} the ${dogBreed}`
+    console.log(dogAction);
+    return dogAction
+  }
+function walkToPark(dogName, dogBreed) {
+    const dogAction = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(dogAction);
+    return dogAction
+  }
+function throwFrisbee(dogName, dogBreed) {
+    const dogAction = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(dogAction);
+    return dogAction
+  }
+function walkHome(dogName, dogBreed) {
+    const dogAction = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(dogAction);
+    return dogAction
+  }
+function unleashDog(dogName, dogBreed) {
+    const dogAction = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(dogAction);
+    return dogAction
+  }
+
+  const routine = [wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    let array = []
+    routine.forEach(f => {
+        array.push(f(dogName, dogBreed))
+    });
+    return array
+}
+
